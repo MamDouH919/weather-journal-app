e7fb8ac5e735e0b4b47b20c7a44c53da21d79fb0
Mamdouh Mohammed El-Maghawry <mamdouh.mohammed919@gmail.com>
2022-02-14 10:44:45 +0200

    init commit

---
+++ website/app.js
@@ -0,0 +1,21 @@
+/* Global Variables */
+const alldata =[]
+const retrieveData = async () =>{
+    const request = await fetch('/all');
+    try {
+    // Transform into JSON
+    const allData = await request.json()
+    console.log(allData)
+    // Write updated data to DOM elements
+    document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
+    document.getElementById('content').innerHTML = allData.feel;
+    document.getElementById("date").innerHTML =allData.date;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+   }
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
